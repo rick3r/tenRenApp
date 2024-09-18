2fd1acfd5af9da21c8cd2ac0e324dfd88f09b1e8
lilo <lilo@d2-00-0.rk.org>
2024-09-17 21:36:16 -0400

    create styler app

---
+++ styler02/App.tsx
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import Flatcards from './components/Flatcards'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Flatcards />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
