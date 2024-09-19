a5f2b5a26e0df58cf6cee72066a390001469a97d
lilo <lilo@d2-00-0.rk.org>
2024-09-19 13:48:12 -0400

    create flatcard tutorial finished

--- styler02/components/Flatcards.tsx
+++ styler02/components/Flatcards.tsx
@@ -3,10 +3,57 @@ import React from 'react'
 
 export default function Flatcards() {
   return (
-    <View>
-      <Text>Flatcards</Text>
+    <View >
+      <Text style={styles.headingText}>Flatcards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+            <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+            <Text>blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+            <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardFour]}>
+            <Text>Yellow</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headingText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    container:{
+        flex:1,
+        flexDirection:'row',
+        padding: 8
+    },
+    card:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 4,
+        margin: 8
+
+    },
+    cardOne:{
+        backgroundColor: 'red'
+    },
+    cardTwo:{
+        backgroundColor: 'blue'
+    },
+    cardThree:{
+        backgroundColor: 'green'
+    },
+    cardFour:{
+        backgroundColor: 'yellow'
+    }
+})
