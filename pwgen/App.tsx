4e96b037c98b919608a30542792b99b31f2f7985
lilo <lilo@d2-00-0.rk.org>
2024-09-20 19:11:04 -0400

    Lesson 22 "Logic to generate password React Native" completed

--- pwgen/App.tsx
+++ pwgen/App.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 //for validation
 import * as Yup from 'yup'
 
 const PasswordSchema = Yup.object().shape({
-    passwordLenght: Yup.number()
+  passwordLenght: Yup.number()
     .min(4, 'Should be a minimum of 4 characters')
     .max(16, 'Should be a maximum of 16 characters')
     .required('Field is required')
@@ -13,11 +13,38 @@ const PasswordSchema = Yup.object().shape({
 
 
 export default function App() {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+  const [lowerCase, setLowerCase] = useState(true)
+  const [uppercase, setUppercase] = useState(false)
+  const [number, setNumber] = useState(false)
+  const [symbol, setSymbol] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) => {
+
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charCodeAt(characterIndex)
+    }
+
+    return result
+  }
+
+}
+
+const resetPassswordState = () => { }
+
+return (
+  <View>
+    <Text>App</Text>
+  </View>
+)
 }
 
 const styles = StyleSheet.create({})
